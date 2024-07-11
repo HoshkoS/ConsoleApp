bce327076f13230343677dc6d82a8c0ab0548697
HoshkoS <sofochka9500ho@gmail.com>
2024-07-11 12:42:29 +0300

    Add retrying attempts

--- main.rb
+++ main.rb
@@ -3,17 +3,27 @@ require 'i18n'
 I18n.load_path << Dir[File.expand_path('locales') + '/*.yml']
 I18n.default_locale = :en
 
-def select_language
-  begin
-    puts I18n.t(:select_language)
-    puts I18n.available_locales.join("/")
+MAX_ATTEMPTS = 5
 
-    choice = gets.chomp.downcase
-    I18n.locale = choice
+def select_language
+  attempt ||= 1
+  puts I18n.t(:select_language)
+  puts I18n.available_locales.join("/")
+
+  choice = gets.chomp.downcase
+  I18n.locale = choice
+
   rescue I18n::InvalidLocale
     puts I18n.t(:invalid_choice)
-    retry
-  end
+
+    if attempt < MAX_ATTEMPTS
+      attempt += 1
+
+      puts I18n.t(:attempt_i, attempt: attempt, max_attempts: MAX_ATTEMPTS)
+      retry
+    else
+      puts I18n.t(:no_attempts_left)
+    end
 end
 
 select_language
