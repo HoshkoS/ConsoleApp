dd46194c12f940d9ec81be92a9b4c3c52964c0cb
HoshkoS <sofochka9500ho@gmail.com>
2024-07-11 13:26:20 +0300

    Add validation for words

--- services/fill_file.rb
+++ services/fill_file.rb
@@ -4,6 +4,15 @@ class FillFile
     puts I18n.t(:enter_words)
     words = gets.chomp.split(',')
 
+    words.map!(&:strip)
+    valid_words = words.all? { |word| word.match?(/\A[a-zA-Z]+\z/) }
+
+    unless valid_words
+      puts I18n.t(:invalid_words)
+      call(path)
+      return
+    end
+
     File.open(path, 'a') do |file|
       words.each { |word| file.puts word.strip }
     end
