4f1422095b32245042a11a3eca67e9a5ed84dce1
HoshkoS <sofochka9500ho@gmail.com>
2024-07-13 21:34:28 +0300

    Add argument to rake task

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,13 @@
 require 'rake'
 require 'fileutils'
 
-task :remove_files do
-  path = 'new_files'
+task :remove_folder , [:folder] do |t, args|
+  args.with_defaults(folder: 'new_files')
+  path = args[:folder]
 
   if Dir.exist?(path)
     FileUtils.rm_rf(path)
-    puts "All new files removed."
+    puts "Folder and files within removed."
   else
     puts "No new files."
   end
